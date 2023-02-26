d198622521556c132bfa2825c7183ebfd34498da
karlaCRM <abraxaskrm@gmail.com>
2023-02-26 14:49:51 -0600

    portafolio karla

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import {Bitter} from '@next/font/google'
+
+
+const bitter = Bitter({
+  subsets:['latin'],
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={bitter.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
